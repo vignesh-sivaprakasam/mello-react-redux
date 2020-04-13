891584d2fed4dc4469169164fda34d8d30566c6a
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-04-13 22:52:36 +0530

    Adding App.js File

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import TopBar from './components/TopBar/TopBar';
+
+class App extends Component {
+  render() {
+    return (
+      <TopBar></TopBar>
+    );
+  }
+}
+
+export default App;
