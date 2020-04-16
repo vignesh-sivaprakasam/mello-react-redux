0ba63fa625684bd1c2792f4ba1579249e7781485
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-04-16 23:51:25 +0530

    changed domain to wprk in live

--- src/domain.js
+++ src/domain.js
@@ -1,2 +1,2 @@
-export const domain = "http://localhost:5000";
-// export const domain = "http://mello-server.herokuapp.com"; 
+// export const domain = "http://localhost:5000";
+export const domain = "http://mello-server.herokuapp.com"; 
