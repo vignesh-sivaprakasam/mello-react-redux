7d7a018ee95a0058f762d699a64b80cb3309ffe7
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-05-02 02:27:26 +0530

    CArd Drag and drop work around

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useRef } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import Menu from '../Menu/Menu';
 import MenuItem from '../Menu/MenuItem';
 
@@ -21,75 +21,69 @@ function Card(props) {
                 setIsMenuOpen(!isMenuOpen);
         }
         const onDeleteMenu = () =>{
-                console.log(" onDeleteMenu : ", props.stackID,  " cardID  :", props.card._id);
                 boardContext.deleteCard(props.stackID, props.card._id);
         }
-
-
 
         const onDragStart = (ev) => {
                 let cardID   = props.card._id;
                 let position = props.order.indexOf(cardID);
-                setDrag({stackID: props.stackID, cardID: cardID, cardDom : cardRef.current, position: position});
+                setDrag({stackID: props.stackID, cardID: cardID, cardDom : cardRef.current, position: position, card: props.card, curStackID: props.stackID});
                 setDrop({stackID: props.stackID, cardID: cardID, position: position});
         }
+        
 
         const onDragEnter = (ev) => {
                 const prevDrop = getDrop();
                 ev.stopPropagation();
-                if(prevDrop.stackID === props.stackID && prevDrop.cardID === props.card._id){
-                        return;
-                } else {
-                        const drag          = getDrag();
-                        const dropCardID    = props.card._id;
-                        const dropStackID   = props.stackID;
-                        const dragCardDom   = drag.cardDom;
-                        let newDropPosition = prevDrop.position;
-                        if(drag.cardID !== dropCardID){
-                                const cardHolder = cardRef.current.parentElement;
-                                const dragOverDom = cardRef.current;
-                                if(dragOverDom.nextElementSibling === dragCardDom){
-                                        if(getDirection() == null || getDirection() === true){
-                                                setDirection(true);
-                                                newDropPosition = prevDrop.position - 1;
-                                        } else if(getDirection() === false){
-                                                if(drag.stackID === prevDrop.stackID){
-                                                        // console.log("decrement");
-                                                        newDropPosition = (prevDrop.position - 1);
-                                                } else {
-                                                        newDropPosition = prevDrop.position;
-                                                }
-                                        }
-                                        cardHolder.insertBefore(dragCardDom, dragOverDom);
-                                } else if(dragCardDom.nextElementSibling === dragOverDom) {
-                                        // console.log("down :");
-                                        if(getDirection() == null || getDirection() === false) {
-                                                setDirection(false);
-                                                newDropPosition = prevDrop.position + 1;
-                                        } else if (getDirection() === true) {
-                                                if(drag.stackID === prevDrop.stackID){
-                                                        newDropPosition = prevDrop.position + 1;
-                                                } else {
-                                                        newDropPosition = prevDrop.position;
-                                                }
-                                        }
-                                        newDropPosition = prevDrop.position + 1;
-                                        cardHolder.insertBefore(dragOverDom, dragCardDom);
-                                } else {
-                                        if(dragCardDom !== dragOverDom){
-                                                // console.log("stack change");
-                                                cardHolder.insertBefore(dragCardDom, dragOverDom);
-                                        }
-                                }
+                
+                const drag          = getDrag();
+                const dropCardID    = props.card._id;
+                const dropStackID   = props.stackID;
+                const dragCardDom   = drag.cardDom;
+                let newDropPosition = prevDrop.position;
+                if(drag.cardID !== dropCardID){
 
-                                if(drag.position === newDropPosition && drag.stackID === dropStackID) {
-                                        // console.log("same pos");
-                                        // dragAndDropState.setDirection(null);
-                                        setDirection(null);
+                        const dragOverDom = cardRef.current;
+                        if(dragOverDom.nextElementSibling === dragCardDom){
+                                if(getDirection() == null || getDirection() === true){
+                                        setDirection(true);
+                                        newDropPosition = prevDrop.position - 1;
+                                } else if(getDirection() === false){
+                                        if(drag.stackID === prevDrop.stackID){
+                                                // console.log("decrement");
+                                                newDropPosition = (prevDrop.position - 1);
+                                        } else {
+                                                newDropPosition = prevDrop.position;
+                                        }
                                 }
-                                setDrop({stackID: props.stackID, cardID: dropCardID, position: newDropPosition});
+                        } else if(dragCardDom.nextElementSibling === dragOverDom) {
+                                if(getDirection() == null || getDirection() === false) {
+                                        setDirection(false);
+                                        newDropPosition = prevDrop.position + 1;
+                                } else if (getDirection() === true) {
+                                        if(drag.stackID === prevDrop.stackID){
+                                                newDropPosition = prevDrop.position + 1;
+                                        } else {
+                                                newDropPosition = prevDrop.position;
+                                        }
+                                }
+                                newDropPosition = prevDrop.position + 1;
+                                // cardHolder.insertBefore(dragOverDom, dragCardDom);
+                        } else {
+                                if(dragCardDom !== dragOverDom){
+                                        newDropPosition = props.order.indexOf(props.card._id)
+                                }
                         }
+
+                        if(drag.position === newDropPosition && drag.stackID === dropStackID) {
+                                setDirection(null);
+                        }
+                        setDrop({stackID: props.stackID, cardID: dropCardID, position: newDropPosition});
+                        boardContext.onCardMove();
+                } else {
+                        console.log("drag cardID == dropCardID");
                 }
+
                 ev.preventDefault();
         }
 
@@ -98,7 +92,6 @@ function Card(props) {
         const onDrop = (ev) => {
                 const drag = getDrag();
                 const drop = getDrop();
-                console.log("drop on card", drop.position);
                 boardContext.moveCard(drag.stackID, drag.cardID, drop.stackID, drop.position);
         }
 
