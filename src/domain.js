c3e391008288bcbdd76e4befa8edbb3b357b4e86
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-05-31 13:14:30 +0530

    Mixed Content from origin -- issue fix

--- src/domain.js
+++ src/domain.js
@@ -1,5 +1,6 @@
 // export const domain = "http://localhost:5000";
-export const domain = "http://mello-server.herokuapp.com"; 
+const protocol = window.location.protocol;
+export const domain   = protocol+"//mello-server.herokuapp.com";
 export const boardUrl = "/api/boards";
 export const stackUrl = "/stacks";
-export const cardUrl = "/cards";
+export const cardUrl  = "/cards";
