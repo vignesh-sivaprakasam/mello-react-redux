8704ae02b9c6a55e5b0dc608a28070aa66a5c53b
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-06-28 12:58:43 +0530

    Changing domain url

--- src/domain.js
+++ src/domain.js
@@ -1,6 +1,6 @@
-export const domain = "http://localhost:5000";
+// export const domain = "http://localhost:5000";
 const protocol = window.location.protocol;
-// export const domain   = protocol+"//mello-server.herokuapp.com";
+export const domain   = protocol+"//mello-server.herokuapp.com";
 export const boardUrl = "/api/boards";
 export const stackUrl = "/stacks";
 export const cardUrl  = "/cards";
